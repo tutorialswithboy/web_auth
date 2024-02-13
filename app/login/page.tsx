eea085e4175e05a1d6a55b91e989f53034629b76
Arunava Basu <arunavabasudev@gmail.com>
2024-02-13 12:02:25 +0530

    fix: add redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from 'next-auth';
 import Form from './form';
+import { redirect } from 'next/navigation';
 
 export default async function LoginPage() {
   const session = await getServerSession();
